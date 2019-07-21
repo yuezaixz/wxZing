1a9bd4263bb7b614faebd77c7df10c61a5ae549f
吴迪玮 <xiao303178394>
2019-07-21 23:52:23 +0800

    活动页面

---
+++ pages/apply/index.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.container
+  .next
+    .title 活动报名
+  
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  middleware: 'wechat-info',
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapState([
+      'authUser'
+    ])
+  },
+
+  methods: {
+  },
+
+  components: {
+  },
+
+  beforeCreate() {
+    // this.$store.dispatch('queryActivityState')
+    // this.$store.dispatch('queryLovers')
+    // this.$store.dispatch('queryFollowers')
+  }
+}
+</script>
+
+<style scoped, lang="sass" src='~/static/css/apply.sass'></style>
