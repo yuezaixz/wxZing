575bb33a9947d8d88a94a7bb653ab8944c3f70ee
吴迪玮 <xiao303178394>
2019-07-23 23:38:25 +0800

    报名成功页面；资料页面框架

---
+++ pages/apply/success.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+
+.container
+  .card
+    .card-header
+      img.card-left-circle(src='~static/img/banner_circle.png')
+      .card-flex-1
+      img.card-bgsjh(src='~static/img/banner_bgsjh.png')
+      img.card-op(src='~static/img/banner_office_planning.png')
+      img.card-close(src='~static/img/banner_close.png')
+
+    .card-body
+      .apply-success-title 报名成功
+      .apply-success-sub-title 已报名活动：表情包与段子手
+      div(style="height:34px;")
+      .card-row(style="justify-content:center;")
+        .apply-cancel-button 
+          .apply-cancel-button-text(@click="cancel_apply") 取消报名
+      div(style="height:28px;")
+      .apply-success-sub-title 工作人员会将您拉入相应的活动群组，请主动添加工作人员微信号，请留意相关活动信息
+      div(style="height:8px;")
+      .card-row(style="justify-content:center;")
+        img.apply-office-qrcode(src='~static/img/office_qrcode.png')
+      .apply-success-qrcode-title 请主动添加工作人员微信
+      div(style="height:20px;")
+
+    .card-footer
+  .next
+    div(@click='next')
+      .title 继续浏览
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+import { setTimeout } from 'timers';
+
+export default {
+  middleware: 'wechat-auth',
+  data() {
+    return {
+      user: {},
+      activeGender:0
+    }
+  },
+
+  computed: {
+    ...mapState([
+      'authUser'
+    ])
+  },
+  beforeMount() {
+    
+  },
+
+  methods: {
+    async cancel_apply() {
+
+    },
+    async next() {
+    }
+  },
+
+  components: {
+  }
+}
+</script>
+
+<style scoped, lang="sass" src='~/static/css/register.sass'></style>
+<style scoped, lang="sass" src='~/static/css/apply_success.sass'></style>
---
+++ pages/zing/index.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.container
+  .card
+    .card-header
+      img.card-close(src='~static/img/banner_close.png' style="margin-left:10px;margin-right:5px;")
+      img.card-bgsjh(src='~static/img/banner_bgsjh.png')
+      img.card-op(src='~static/img/banner_office_planning.png')
+      .flex-1
+      .card-header-filter(@click="filter" style="padding-right:20px;" )
+        .card-header-filter-title 筛选
+        img.card-arrow-down(src='~static/img/arrow_down.png')
+
+    .card-body
+      .card-column(style='height:30px;')
+
+    .card-footer
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  middleware: 'wechat-info',
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    ...mapState([
+      'authUser',
+    ])
+  },
+
+  methods: {
+    displayUserId(userId) {
+      return (Array(6).join(0) + userId).slice(-6)
+    },
+    filter() {
+
+    }
+  },
+
+  components: {
+  },
+
+  beforeCreate() {
+    // this.$store.dispatch('queryActivityState')
+  }
+}
+</script>
+
+<style scoped, lang="sass" src='~/static/css/zing.sass'></style>
