f6489f6f73fea238889965d190ef3574e1bfeb2a
吴迪玮 <xiao303178394>
2019-12-15 22:00:08 +0800

    fix

--- middleware/wechat-autologin.js
+++ middleware/wechat-autologin.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import config from '../config'
 
 export default function ({ commit, store, route, redirect }) {
   if (!store.state.authUser) {
@@ -7,7 +8,7 @@ export default function ({ commit, store, route, redirect }) {
     return redirect(`/wechat-redirect?visit=${fullPath}`)
   } else {
     if (store.state.authUser && store.state.authUser.unionid) {
-      axios.get('http://paodong.vipgz1.idcfengye.com/api/user/auto_login/' + store.state.authUser.unionid).then((loginRes) => {
+      axios.get(config.SITE_ROOT_URL + '/api/user/auto_login/' + store.state.authUser.unionid).then((loginRes) => {
         if (loginRes.data.success) {
           let user = loginRes.data.data
           store.state.authUser = user
