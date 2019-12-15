382afbf0084b7ecd4335e80e82160efa1d3f0090
吴迪玮 <xiao303178394>
2019-12-15 22:17:21 +0800

    fix

--- middleware/wechat-autologin.js
+++ middleware/wechat-autologin.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import config from '../server/config'
 
 export default function ({ commit, store, route, redirect }) {
   if (!store.state.authUser) {
@@ -8,7 +7,7 @@ export default function ({ commit, store, route, redirect }) {
     return redirect(`/wechat-redirect?visit=${fullPath}`)
   } else {
     if (store.state.authUser && store.state.authUser.unionid) {
-      axios.get(config.SITE_ROOT_URL + '/api/user/auto_login/' + store.state.authUser.unionid).then((loginRes) => {
+      axios.get('http://wxzing.officeplan.cn/api/user/auto_login/' + store.state.authUser.unionid).then((loginRes) => {
         if (loginRes.data.success) {
           let user = loginRes.data.data
           store.state.authUser = user
